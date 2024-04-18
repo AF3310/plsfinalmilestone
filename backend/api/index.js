e5e6514809735f5bc64e2302e76d190eb6b4c4bd
AF3310 <88600358+AF3310@users.noreply.github.com>
2024-04-18 19:40:41 +0200

    dd

--- backend/api/index.js
+++ backend/api/index.js
@@ -5,8 +5,14 @@ const cors=require('cors')
 
 const app=express()
 
-app.use(cors()); // Allow requests from all origins
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
+app.use(cors(corsOptions)) // Use this after the variable declaration
 app.use(express.json()); 
 
 const db = mysql.createConnection({
